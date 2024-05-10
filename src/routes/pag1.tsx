4a0253fd9af5c0c45b952ddd8463c29873d4f65f
Martí Pardo <57571130+Mapaor4@users.noreply.github.com>
2024-05-10 17:05:19 +0200

    Delete src/routes/pag1.tsx

--- src/routes/pag1.tsx
+++
@@ -1,9 +0,0 @@
-export default function pagina1() {
-  return (
-    <main>
-      <Title>Pàgina 1</Title>
-      <h1>Això és una pàgina qualsevol</h1>
-      <p>Com pinta tot plegat?</p>
-    </main>
-  );
-}
